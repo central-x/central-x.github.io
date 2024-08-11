4507876d1a20a6a49e74a4eaa14a2b3d96b09b11
Alan Yeh <alan@yeh.cn>
2024-08-11 23:17:46 +0800

    [UPDATE] 集成 Disqus 评论系统
    Signed-off-by: Alan Yeh <alan@yeh.cn>

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,6 +2,7 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import Disqus from './components/Disqus.vue'
 import './style.css'
 
 export default {
@@ -9,6 +10,7 @@ export default {
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      'doc-after': () => h(Disqus)
     })
   },
   enhanceApp({ app, router, siteData }) {
